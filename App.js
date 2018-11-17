b4d6d21ab2ca7d3883f08b33693b58a41cc1bcd5
John Angel <j_angeldiaz@hotmail.com>
2018-11-17 15:39:07 -0500

    Components creation

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import DeckList from './components/DeckList'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <DeckList style={styles.container}/>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
